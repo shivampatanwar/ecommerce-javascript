ed2cdb1b5826bbfecb0bc195257ee636b011d063
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-08-23 20:37:34 +0530

    s

--- general/js/hamberger.js
+++ general/js/hamberger.js
@@ -1,20 +1,15 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  let hamberger = document.getElementById('hamberger');
-  let nav = document.getElementById('nav');
+    let hamberger = document.getElementById('hamberger');
+    let nav = document.getElementById('nav');
 
-  // Initialize nav display style
-  nav.style.display = 'none';
+    hamberger.addEventListener('click', () => {
+        nav.style.display = nav.style.display === 'block' ? 'none': 'block';  
 
-  hamberger.addEventListener('click', (event) => {
-    event.stopPropagation();
-    nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
-  });
+    });
 
-  document.addEventListener('click', (event) => {
-    if (nav.style.display === 'block' && !nav.contains(event.target) && event.target !== hamberger) {
-      nav.style.display = 'none';
-    }
-  });
+
+
+
 });
 
