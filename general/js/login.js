2c1b04bc2f1cf014e0a2e5bb172728887ac5fb1d
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-08-26 20:40:05 +0530

    s

--- general/js/login.js
+++ general/js/login.js
@@ -20,30 +20,59 @@ loginbtn.addEventListener('click', (e) => {
     let merchants = JSON.parse(localStorage.getItem('merchant'));
     let admins = JSON.parse(localStorage.getItem('admin'));
 
-    let customer = customers.find(c => c.email === email)
-    let merchant = merchants.find(m => m.email === email)
-    let admin = admin.find(a => a.email === email)
 
 
-    //check if the customer email and password are correct
-    if (customer.email === email && customer.password === password) {
-        //redirect to customer dashboard
-        location.href = "customer-home.html";
 
-    }
-    //check if the merchant email and password are correct
-    else if (merchant.email === email && merchant.password === password) {
-        //redirect to merchant dashboard
-        location.href = "merchant-home.html";
-    }
-    //check if the admin email and password are correct
-    else if (admin.email === email && admin.password === password) {
-        //redirect to admin dashboard
-        location.href = "admin-home.html";
-    }
-    //if not, show error message
-    else {
-        alert('Invalid email or password');
+    // if no customer, merchant or admin array found in local storage, show error message
+    if (!customers && !merchants && !admins) {
+        alert('No customer, merchant or admin found in local storage');
+        return;
+    } else {
+
+        if (customers) {
+            //check if the customer email and password are correct
+            let customer = customers.find(c => c.email === email);
+            if (customer.email === email && customer.password === password) {
+                //redirect to customer dashboard
+                location.href = "customer-home.html";
+            }else if ( customer.email !== email) {
+                alert('Incorrect Email');
+            }
+            else {
+                alert('Incorrect Password');
+            }
+
+        }
+        else if (merchants) {
+            let merchant = merchants.find(m => m.email === email);
+            //check if the merchant email and password are correct
+            if (merchant.email === email && merchant.password === password) {
+                //redirect to merchant dashboard
+                location.href = "merchant-home";
+            }else if ( merchant.email !== email) {
+                alert('Incorrect Email');
+            }
+            else {
+                alert('Incorrect Password');
+            }
+        }
+        else if (admins) {
+            let admin = admins.find(a => a.email === email);
+            //check if the admin email and password are correct
+            if (admin.email === email && admin.password === password) {
+                //redirect to admin dashboard
+                location.href = "admin-home";
+            }
+            else if ( admin.email !== email) {
+                alert('Incorrect Email');
+            }
+            else {
+                alert('Incorrect Password');
+            }
+        }
+        else {
+            alert('Invalid email or password');
+        }
     }
 
 });
