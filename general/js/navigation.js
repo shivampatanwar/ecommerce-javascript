8c681f4e590fdb9ded789be34cae8cdedde68288
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-08-26 22:23:55 +0530

    a

---
+++ general/js/navigation.js
@@ -0,0 +1,71 @@
+
+
+let home = document.getElementById('home');
+
+
+let cart = document.getElementById('cart');
+let order = document.getElementById('order');
+let customerprofile = document.getElementById('customerprofile');
+
+let admincustomer = document.getElementById('admincustomer');
+let adminmerchant = document.getElementById('adminmerchant');
+let adminprofile = document.getElementById('adminprofile');
+
+
+let product = document.getElementById('product');
+let currentorder = document.getElementById('currentorder');
+let previousorder = document.getElementById('previousorder');
+let merchantprofile = document.getElementById('merchantprofile');
+
+let signup = document.getElementById('signup');
+let login = document.getElementById('login');
+let logout = document.getElementById('logout');
+
+
+// Customer
+cart.style.display = 'none';
+order.style.display = 'none';
+customerprofile.style.display = 'none';
+
+
+//Admin
+admincustomer.style.display = 'none';
+adminmerchant.style.display = 'none';
+adminprofile.style.display = 'none';
+
+
+//Merchant
+product.style.display = 'none';
+currentorder.style.display = 'none';
+previousorder.style.display = 'none';
+merchantprofile.style.display = 'none';
+
+//login and signup
+logout.style.display = 'none';
+
+
+
+
+let loginas = JSON.parse(localStorage.getItem('loginas'));
+
+if(loginas==='customer'){
+    cart.style.display = 'block';
+    order.style.display = 'block';
+    customerprofile.style.display = 'block';
+    logout.style.display = 'block';
+
+}else if(loginas==='merchant'){
+    merchantproduct.style.display = 'block';
+    merchantcurrentorder.style.display = 'block';
+    merchantpreviousorder.style.display = 'block';
+    merchantprofile.style.display = 'block';
+    logout.style.display = 'block';
+
+}else if(loginas==='admin'){
+    admincustomer.style.display = 'block';
+    adminmerchant.style.display = 'block';
+    adminprofile.style.display = 'block';
+    logout.style.display = 'block';
+}
+
+
