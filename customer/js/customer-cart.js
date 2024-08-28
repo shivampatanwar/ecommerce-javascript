ed325f553976feae85d62d0968c2fb83a8af1e56
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-08-28 23:40:08 +0530

    w

--- customer/js/customer-cart.js
+++ customer/js/customer-cart.js
@@ -39,9 +39,9 @@ function displayProduct(element) {
                     <p class="price">Price: &#8377; ${element.price.toFixed(2)}</p>
                     <p class="total">Total Price: &#8377; ${element.totalprice.toFixed(2)}</p>
                    <div class="idbtn">
-                        <button class="increase" onclick="increase(${element.id})">-</button>
-                        <p>${element.quantity}</p>
-                        <button class="decrease" onclick="decrease(${element.id})">+</button>
+                   <button class="decrease" onclick="decrease(${element.id})">-</button>
+                   <p>${element.quantity}</p>
+                   <button class="increase" onclick="increase(${element.id})">+</button>
                    </div>
 
                     <button class="remove cddiv" onclick="removeFromCart(${element.id})">Delete</button>
@@ -58,4 +58,42 @@ function displayProduct(element) {
         productContainer.innerHTML = `<h2>No products found</h2>`;
     }
 
+}
+
+
+
+function increase(id) {
+    let customer = customers.find(c => c.id === loginid);
+    let cart = customer.cart;
+    let product = cart.find(p => p.id === id);
+    product.quantity++;
+    product.totalprice = product.price * product.quantity;
+    localStorage.setItem('customer', JSON.stringify(customers));
+    location.reload();
+}
+
+function decrease(id) {
+    let customer = customers.find(c => c.id === loginid);
+    let cart = customer.cart;
+    let product = cart.find(p => p.id === id);
+    if (product.quantity > 1) {
+        product.quantity--;
+        product.totalprice = product.price * product.quantity;
+        localStorage.setItem('customer', JSON.stringify(customers));
+        location.reload();
+    }else{
+        removeFromCart(id);
+    }
+}
+
+function removeFromCart(id) {
+    let customer = customers.find(c => c.id === loginid);
+    let cart = customer.cart;
+    let product = cart.find(p => p.id === id);
+    let index = cart.indexOf(product);
+    if (index !== -1) {
+        cart.splice(index, 1);
+        localStorage.setItem('customer', JSON.stringify(customers));
+        location.reload();
+    }
 }
